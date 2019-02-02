8381d81339d27774caffde883a2785e48082b203
Philipp Bosch <hello@pb.io>
2019-02-02 13:52:29 +0100

    Add Brewfile, bootstrap.sh (WIP).

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+brew tap caskroom/cask
+brew cleanup
+
+# Check Homebrew installation so far
+brew doctor
+read -p "Press Enter if the above "
+
+# Install brews
+brew bundle install
