b35164ab485bdfdb83a86b72793e628b4257cd04
Philipp Bosch <hello@pb.io>
2019-02-03 09:57:22 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Install Xcode Command Line Tools
-xcode-select --install
-
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
