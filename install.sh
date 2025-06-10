3d4342518a29e15ab187d0b0cd3c3874e754cef6
Philipp Bosch <hello@pb.io>
2025-06-10 23:04:07 +0200

    oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Add oh-my-zsh as a submodule
+if [ ! -d home/oh-my-zsh ]
+then
+    git submodule add https://github.com/ohmyzsh/ohmyzsh.git home/oh-my-zsh
+fi
+
+# Update oh-my-zsh
+git submodule update --init --recursive --remote
+
+# Backup existing zshrc
+if [ -f "$HOME/.zshrc" ]
+then
+    mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+fi
+
+# Link all files in home to the home directory
 for file in $(ls home)
 do
     if [[ -f "$HOME/.$file" ]]
